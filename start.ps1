ad535f13d247725bf680b7777b8c654cbd838239
Gleb Golubev <marmotik.1973@gmail.com>
2024-08-08 17:05:10 +0300

    added start script

--- start.ps1
+++ start.ps1
@@ -1,4 +1,4 @@
 $scriptDir = Join-Path -Path ([Environment]::GetFolderPath("Desktop")) -ChildPath "pdfgen"
 Set-Location $scriptDir
 Start-Process -FilePath "npm" -ArgumentList "start" -Wait
-Start-Process -FilePath "explorer" -ArgumentList "http://localhost:3000"
+Start-Process -FilePath "chrome" -ArgumentList "http://localhost:3000"
